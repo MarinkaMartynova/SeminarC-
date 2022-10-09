57e268fb30f0c7b80d3181e1745d5507c8f8d914
Marina <saramsok@gmail.com>
2022-10-09 14:42:30 +0400

    add Task 21

---
+++ Task_021/Program.cs
@@ -0,0 +1,18 @@
+﻿/* Напишите программу, которая принимает на вход координаты двух точек
+и нахоидт расстояние между ними в 2D пространстве
+
+ //теорема Пифагора c^2 = a^2 + b^2
+ //таким образом c = квадртаному корню(a^2 + b^2)
+ */
+
+Console.Write("Введите x1:");
+int x1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите y1:");
+int y1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите x2:");
+int x2 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите y2:");
+int y2 = Convert.ToInt32(Console.ReadLine());
+double distance = 0;
+distance = Math.Sqrt((x1 - x2) * (x1 - x2) + (y1 - y2) * (y1 - y2));
+Console.WriteLine("Расстояние между точками равно: " + Math.Round(distance, 2));

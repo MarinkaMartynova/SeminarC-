90c493a166537e743c2720912845f843135936d5
Marina <saramsok@gmail.com>
2022-10-09 14:41:32 +0400

    Add Task 17

---
+++ Task_017/Program.cs
@@ -0,0 +1,22 @@
+﻿/* 17. Напишите программу, которая принимает на вход координаты точки (X и Y),
+причем X ≠ 0 и Y ≠ 0 и выдает номер четверит плоскости, 
+в которой находится эта точка
+*/
+
+Console.Write("Введите x: ");
+int x = Convert.ToInt32 (Console.ReadLine());
+Console.Write("Введите y: ");
+int y = Convert.ToInt32(Console.ReadLine());
+
+if (x > 0 && y > 0) {
+    Console.WriteLine("Первая четверть");
+}
+if (x > 0 && y < 0) {
+    Console.WriteLine("Вторая четверть");
+}
+if (x < 0 && y > 0) {
+    Console.WriteLine("Третья четверть");
+}
+if (x < 0 && y < 0) {
+    Console.WriteLine("Четвертая четверть");
+}

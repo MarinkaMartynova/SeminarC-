4d065ca61780f766ac24fff577831e93778d0c0d
Marina <saramsok@gmail.com>
2022-10-15 21:14:26 +0400

    add Homework_027

---
+++ Homework_027/Program.cs
@@ -0,0 +1,14 @@
+﻿/*Задача 27: Напишите программу, которая принимает на вход число 
+и выдаёт сумму цифр в числе.
+452 -> 11
+82 -> 10
+9012 -> 12
+*/
+Console.Write("Enter the number of:  ");
+int number = int.Parse(Console.ReadLine());
+int sum = 0;
+for(int i = number; i > 0; i--) {
+    sum = sum + number % 10;
+    number = number / 10;    
+}
+Console.WriteLine(sum); 

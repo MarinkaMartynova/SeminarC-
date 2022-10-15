dce1cf1299497b8322cf46f7118e9544b81d5c29
Marina <saramsok@gmail.com>
2022-10-15 21:14:15 +0400

    add Homework_025

---
+++ Homework_025/Program.cs
@@ -0,0 +1,10 @@
+﻿/*Задача 25: Напишите цикл, который принимает на вход два числа (A и B) 
+и возводит число A в натуральную степень B.
+                 3, 5 -> 243 (3⁵)
+                 2, 4 -> 16
+*/
+Console.Write("Введите число:   ") ;
+int num = int.Parse(Console.ReadLine()) ; //num - число
+Console.Write("Введите сетпень: ") ;
+int power = int.Parse(Console.ReadLine()) ; //power- степень
+Console.WriteLine(Math.Pow(num,power));

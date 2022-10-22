db7760befe6e14b7fa44e6211afafeeba89f0982
Marina <saramsok@gmail.com>
2022-10-22 22:54:01 +0400

    add Task 032

---
+++ Task_032/Program.cs
@@ -0,0 +1,58 @@
+﻿/* Задача 32: 
+Напишите программу замены элементов массива: 
+положительные элементы замените на соответствующие отрицательные, и наоборот.
+[-4,-8,8,2] -> [4,8,-8,-2]
+*/
+
+
+//мой вариант
+///*
+int[] array = new int[4];
+
+for(int i = 0; i < array.Length; i++) {
+    
+    array[i] = new Random().Next(-10,10);
+    Console.Write(array[i] + ", ");
+}
+Console.WriteLine();
+for(int i = 0; i < array.Length; i++) {
+    
+    array[i] =  array[i] * -1;
+    Console.Write(array[i] + ", ");
+}
+//*/
+
+
+// пример с семинара
+/*
+int[] array = new Int32 [4];
+
+randomFilling(array, -9, 10);
+arrayOutput(array);
+signChange(array);
+arrayOutput(array);
+
+void signChange(int[] arr) {
+
+    for ( int i = 0; i < arr.Length; i++) {
+        arr[i] = arr[i] * -1;
+    }    
+}
+
+void randomFilling(int[] arr, int min, int max) {
+
+    for ( int i = 0; i < arr.Length; i++) {
+        arr[i] = new Random().Next(min, max);
+    }   
+}
+
+void arrayOutput(int[] arr) {
+
+    for ( int i = 0; i < arr.Length; i++) {
+
+        Console.Write($"{arr[i]}");
+        if(i < (arr.Length - 1)) Console.Write($", ");        
+    }
+    Console.WriteLine();
+}
+*/
---
+++ Task_033/Program.cs
@@ -0,0 +1,86 @@
+﻿/* Задача 33: 
+Задайте массив. Напишите программу, которая определяет, 
+присутствует ли заданное число в массиве.
+4; массив [6,7,19,345,3] -> нет
+-3; массив [6,7,19,345,3] -> да
+*/
+
+
+//мой вариант
+///*
+int[] array = new int[5];
+bool exist = false;
+
+Console.Write("Какое число проверить?:  ");
+int n = Convert.ToInt32(Console.ReadLine()); 
+
+for(int i = 0; i < array.Length; i++) {
+    
+    array[i] = new Random().Next(0,10);
+    Console.Write(array[i] + ", ");
+}
+Console.WriteLine();
+
+for(int i = 0; i < array.Length; i++) {
+    
+    if(n == array[i]) {
+        exist = true;
+    }
+}
+if(exist == true){
+    Console.Write("Значение присутствует в массиве");
+}
+else {
+    Console.Write("Значение отсутствует в массиве");
+}
+//*/
+
+//пример с семинара
+/*
+int[] array = new Int32[10];
+bool exist = false;
+newArray(array);
+PrintArray(array);
+
+void newArray(int[] array, min, max) {
+for(int i = 0; i < array.Length; i++) {
+    
+    Console.Write("Введите значение массива: ");
+    int a = Convert.ToInt32(Console.ReadLine()); 
+    array[i] = a;
+    //array[i] = new Random().Next(0,10);
+    //Console.Write(array[i] + ", ");
+}
+}
+void PrintArray(int[] array){
+    for ( int i = 0; i < array.Length; i++) {
+        System.Console.Write($"{array[i]}, ");
+    }
+    System.Console.Write($"{array[array.Length - 1]}, ");
+
+}
+Console.WriteLine();*/
+
+
+
+/*
+newArray(array);
+PrintArray(array);
+
+Console.WriteLine("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine()); 
+
+string result = Poisk(array, number)? $"Число {number} входит в массив" : $"Число {number} не входит в массив";
+Console.WriteLine(result);
+
+bool Poisk(int[]array, int n) {
+    bool h = false;
+    for (int i = 0; i < array.Length; i++) {
+        if(array[i] == n) {
+            h = true;
+            break;
+        }
+    }
+    return h;
+}
+*/

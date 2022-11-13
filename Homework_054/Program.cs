8c7bb378963dc362d3dfa7a92775125f101c10c3
Marinka <saramsok@gmail.com>
2022-11-13 14:08:48 +0400

    update homework 054

--- Homework_054/Program.cs
+++ Homework_054/Program.cs
@@ -66,7 +66,37 @@ void SortRowsDescending(int[,] array){
         }
     }
 }
+/*
+//сортировка по возрастанию по столбцам
+void SortColumnAscending(int[,] array){
+    for(int j = 0; j < array.GetLength(1); j++){
+        for(int i = 0; i < array.GetLength(0); i++){
+            for(int t = i + 1; t < array.GetLength(0); t++){
+                if(array[i, j] > array[t, j]){
+                    int temp = array[i, j];
+                    array[i, j] = array[t, j];
+                    array[t, j] = temp;
+                }              
+            }
+        }
+    }
+}
 
+//сортировка по убыванию по столбцам
+void SortColumnDescending(int[,] array){
+    for(int j = 0; j < array.GetLength(1); j++){
+        for(int i = 0; i < array.GetLength(0); i++){
+            for(int t = i + 1; t < array.GetLength(0); t++){
+                if(array[i, j] < array[t, j]){
+                    int temp = array[i, j];
+                    array[i, j] = array[t, j];
+                    array[t, j] = temp;
+                }              
+            }
+        }
+    }
+}
+*/
 
 FillArray (array);
 PrintArray(array);
@@ -77,3 +107,11 @@ Console.WriteLine();
 SortRowsDescending(array);
 PrintArray(array);
 Console.WriteLine();
+/*
+SortColumnAscending(array);
+PrintArray(array);
+Console.WriteLine();
+SortColumnDescending(array);
+PrintArray(array);
+Console.WriteLine();
+*/
